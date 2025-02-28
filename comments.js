4e45ed1f8890f32349db4448772e48046c82b2ec
harshvardhanDots <harshvardhan.singh@dotsquares.com>
2025-02-28 13:01:01 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+//create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const commentsPath = path.join(__dirname, 'comments.json');
+const comments = require('./comments.json');
+const port = 3000;
